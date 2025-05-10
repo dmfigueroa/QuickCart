19cdb1b99724c9549baee23ec9eb61ef1b78a384
David Figueroa <davidfigueroa0327@gmail.com>
2025-05-09 19:04:24 -0700

    no parenthesis

--- main.rb
+++ main.rb
@@ -10,14 +10,14 @@ inventory = Inventory.new
 puts inventory
 puts "------------------------------\n"
 
-customer1 = Customer.new({ name: 'John Doe', email: 'john.doe@example.com', address: '123 Main St, Anytown, USA' })
+customer1 = Customer.new name: 'John Doe', email: 'john.doe@example.com', address: '123 Main St, Anytown, USA'
 puts customer1
 
-order1 = OrderProcessor.new(inventory, customer1)
-order1.add_item(inventory.find_item('ITEM001'), 2)
-order1.add_item(inventory.find_item('ITEM003'), 1)
+order1 = OrderProcessor.new inventory, customer1
+order1.add_item inventory.find_item('ITEM001'), 2
+order1.add_item inventory.find_item('ITEM003'), 1
 
-card1 = Card.new('VALID_CARD_NUMBER', '12/25', '123')
+card1 = Card.new '1234567890123456', '12/25', '123'
 if order1.process_payment(card1)
   puts 'Order 1 processing complete.'
 else
@@ -26,15 +26,11 @@ end
 
 puts "\nUpdated Inventory: \n#{inventory}\n\n"
 
-customer2 = Customer.new({
-                           name: 'Jane Smith',
-                           email: 'jane.smith@example.com',
-                           address: '456 Oak Ave, Otherville, USA'
-                         })
+customer2 = Customer.new name: 'Jane Smith', email: 'jane.smith@example.com', address: '456 Oak Ave, Otherville, USA'
 
-order2 = OrderProcessor.new(inventory, customer2)
-order2.add_item(inventory.find_item('ITEM002'), 1) # Mega Gadget
-card2 = Card.new('INVALID_CARD_NUMBER', '01/24', '456')
-order2.process_payment(card2) # This will fail
+order2 = OrderProcessor.new inventory, customer2
+order2.add_item inventory.find_item('ITEM002'), 1 # Mega Gadget
+card2 = Card.new 'INVALID_CARD_NUMBER', '01/24', '456'
+order2.process_payment card2 # This will fail
 
 puts "\nFinal Inventory: \n#{inventory}\n\n"
